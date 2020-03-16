94382f2eefe702a6e921a365d0b51b17006dcbb9
Lucas Oliveira <lu4cap@gmail.com>
2020-03-15 21:54:18 -0300

    CreateTables

---
+++ AgendamentoCore/Models/LogError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendamentoCore.Models
+{
+    public class LogError
+    {
+    }
+}
